7ee887f5a2ecb25b4203dfe1890f04839046ea9b
natsuokawai <kawai.natsuo@gmail.com>
2023-07-02 22:39:21 +0900

    Tokenize input string

--- main.swift
+++ main.swift
@@ -1,14 +1,56 @@
 import Foundation
 
+enum Token {
+    case num(Int)
+    case op(String)
+    case eof
+}
+
+struct Lexer {
+    let string: String
+
+    init(string input: String) {
+        string = input
+    }
+
+    func tokenize() -> [Token] {
+        var tokens: [Token] = []
+
+        let s = Scanner(string: string)
+        if let value = s.scanInt() {
+            tokens.append(Token.num(value))
+        } else {
+            print("Error: Please enter a number.")
+        }
+
+        while (!s.isAtEnd) {
+            if let op = s.scanCharacters(from: CharacterSet(charactersIn: "+-")) {
+                tokens.append(Token.op(op))
+            } else {
+                print("Error: Expected an operator.")
+                break
+            }
+
+            if let value = s.scanInt() {
+                tokens.append(Token.num(value))
+            } else {
+                print("Error: Please enter a number.")
+                break
+            }
+        }
+
+        if s.isAtEnd { tokens.append(Token.eof) }
+
+        return tokens
+    }
+}
+
 func repl() {
     while true {
         print("> ", terminator: "")
-        let line = readLine()!
-        let s = Scanner(string: line)
-        if let value = s.scanInt() {
-            print(value)
-        } else {
-            print("Error: Please enter a number.")
+        if let line = readLine() {
+            let l = Lexer(string: line)
+            print(l.tokenize())
         }
     }
 }
